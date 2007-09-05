2d65c0a9c66509a7b9e5c761b17d0e79f8e6d894
Nicolas Capit <capitn@users.gforge.inria.fr>
2007-09-05 13:40:55 +0000

    (svn merge  -r 749:751 ../branch-2.2/)

--- debian/oar-node.init.d
+++ debian/oar-node.init.d
@@ -17,13 +17,13 @@ OAR_SSHD_CONF="/etc/oar/sshd_config"
 SSHD_OPTS="-f $OAR_SSHD_CONF -o PidFile=/var/lib/oar/oar_sshd.pid"
 
 start_oar_node() {
-    echo "* Edit start_oar_node function in /etc/default/oar-node if you want"
-    echo "  to perform a specific action (like switch the node to Alive)"
+    echo " * Edit start_oar_node function in /etc/default/oar-node if you want"
+    echo "   to perform a specific action (e.g. to switch the node to Alive)"
 }
 
 stop_oar_node() {
-    echo "* Edit stop_oar_node function in /etc/default/oar-node if you want"
-    echo "  to perform a specific action (like switch the node to Absent)"
+    echo " * Edit stop_oar_node function in /etc/default/oar-node if you want"
+    echo "   to perform a specific action (e.g. to switch the node to Absent)"
 }
 
 # Include oar defaults if available
@@ -35,12 +35,12 @@ set -e
 
 case "$1" in
   start)
-    echo "Starting $DESC:"
+    echo " ** Starting $DESC:"
     if [ -f "$OAR_SSHD_CONF" ] ; then
         if start-stop-daemon --start -N "-20" --quiet -c oar --pidfile /var/lib/oar/oar_sshd.pid --exec /usr/sbin/sshd -- $SSHD_OPTS; then
-            echo "* Specific OAR sshd daemon started."
+            echo " * OAR dedicated SSH server started."
         else
-            echo "* ERROR starting the specific OAR sshd daemon."
+            echo " * Failed to start OAR dedicated SSH server."
         fi
     fi
     start_oar_node
@@ -49,9 +49,9 @@ case "$1" in
     echo "Stopping $DESC: "
     if [ -f "$OAR_SSHD_CONF" ] ; then
         if start-stop-daemon --stop --quiet --pidfile /var/lib/oar/oar_sshd.pid; then
-            echo "* Specific OAR sshd daemon stopped."
+            echo " * OAR dedicated SSH server stopped."
         else
-            echo "* ERROR stopping the specific OAR sshd daemon."
+            echo " * Failed to stop OAR dedicated SSH server."
         fi
     fi
     stop_oar_node
