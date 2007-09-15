20a66c22f19ca06a531f6cdd2b57f0c7cd358438
Nicolas Capit <capitn@users.gforge.inria.fr>
2007-09-15 10:09:35 +0000

    debug xauth path for the different linux distributions... if xauth is not installed then the default is /usr/bin/xauth

--- Tools/configurator_wrapper.sh
+++ Tools/configurator_wrapper.sh
@@ -6,9 +6,11 @@ OARUSER=
 
 PERL5LIB=$OARDIR
 OARCONFFILE=/etc/oar/oar.conf
+OARXAUTHLOCATION=
 export PERL5LIB
 export OARDIR
 export OARUSER
 export OARCONFFILE
+export OARXAUTHLOCATION
 
 exec $OARDIR/`basename $0` "$@"
