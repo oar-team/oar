192c814695e4400312beebf91c0fdaf4552ae8ef
Pierre Neyron <neyron@users.gforge.inria.fr>
2008-02-01 17:25:01 +0000

    mysql initial data

---
+++ DB/mysql/default_data.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO queues (queue_name, priority, scheduler_policy) VALUES ('admin','10','oar_sched_gantt_with_timesharing');
+INSERT IGNORE INTO queues (queue_name, priority, scheduler_policy) VALUES ('default','2','oar_sched_gantt_with_timesharing');
+INSERT IGNORE INTO queues (queue_name, priority, scheduler_policy) VALUES ('besteffort','0','oar_sched_gantt_with_timesharing');
+
+INSERT IGNORE INTO gantt_jobs_predictions (moldable_job_id , start_time) VALUES ('0','0');
+
