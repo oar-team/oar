81a1934973da153e1d68e8624155350f022d7069
Pierre Neyron <neyron@users.gforge.inria.fr>
2008-03-31 20:17:26 +0000

    fix regex

--- DB/pg_default_admission_rules.sql
+++ DB/pg_default_admission_rules.sql
@@ -22,7 +22,7 @@ my $admin_group = "admin";
 if ($queue_name eq "admin") {
     my $members; 
     (undef,undef,undef, $members) = getgrnam($admin_group);
-    my %h = map { $_ => 1 } split(/\s/,$members);
+    my %h = map { $_ => 1 } split(/\\s+/,$members);
     if ( $h{$user} ne 1 ) {
         {die("[ADMISSION RULE] Only member of the group ".$admin_group." can submit jobs in the admin queue\\n");}
     }
