0a2d2e4f548c7a7f31eb1e0bd08a272ff1e520d9
Pierre Neyron <neyron@users.gforge.inria.fr>
2008-10-08 15:28:48 +0000

    moved tests

--- Tests/scheduler/data/default_data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('admin','10','oar_sched_gantt_with_timesharing');
-INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('default','2','oar_sched_gantt_with_timesharing');
-INSERT INTO queues (queue_name, priority, scheduler_policy) VALUES ('besteffort','0','oar_sched_gantt_with_timesharing');
-INSERT INTO gantt_jobs_predictions (moldable_job_id , start_time) VALUES ('0','0');
