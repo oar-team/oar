1b5a183c7a3cf3bc1eafc48bf2e4c7bc877c222c
Olivier Richard <auguste@users.gforge.inria.fr>
2009-06-08 09:26:43 +0000

    directory for official test suite

--- API/drmaa-rest/fake-server/app.rb
+++ API/drmaa-rest/fake-server/app.rb
@@ -1,5 +1,14 @@
+require 'rubygems'
 require 'sinatra'
+require 'json'
+
+enable :logging
 
 get '/' do
 	'Woot woot...'
+end
+
+get '/hello' do
+  content_type 'text/json'
+  {:id => 1, :foo => 'bar'}.to_json
 end
