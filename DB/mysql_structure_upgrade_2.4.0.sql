4f1e2a35d17daa5d6b9d37eb0970016c9e93c3fc
Joseph Emeras <emeras@users.gforge.inria.fr>
2009-08-27 15:03:37 +0000

    added version for db scheme

--- DB/mysql_structure_upgrade_2.4.0.sql
+++ DB/mysql_structure_upgrade_2.4.0.sql
@@ -1,1 +1,2 @@
 alter table resources change cm_availability available_upto INT UNSIGNED DEFAULT 0 NOT NULL;
+update `schema` set version='2.4';
--- DB/pg_structure_upgrade_2.4.0.sql
+++ DB/pg_structure_upgrade_2.4.0.sql
@@ -1,1 +1,3 @@
 alter table resources rename column cm_availability to available_upto;
+update schema set version='2.4';
+
