72ca9214e4dbc50382b653ec1a669f3e1f23aa0f
Joseph Emeras <emeras@users.gforge.inria.fr>
2009-09-08 14:08:56 +0000

    added last_available_upto field in the database

--- DB/mysql_structure_upgrade_2.4.0.sql
+++ DB/mysql_structure_upgrade_2.4.0.sql
@@ -1,2 +1,3 @@
 alter table resources change cm_availability available_upto INT UNSIGNED DEFAULT 0 NOT NULL;
+alter table resources add last_available_upto INT UNSIGNED DEFAULT 0 NOT NULL;
 update `schema` set version='2.4';
--- DB/pg_structure_upgrade_2.4.0.sql
+++ DB/pg_structure_upgrade_2.4.0.sql
@@ -1,3 +1,4 @@
 alter table resources rename column cm_availability to available_upto;
+alter table resources add last_available_upto integer NOT NULL default '0';
 update schema set version='2.4';
 
