dafd3f0d5d4e33f62ceab9f830eb2f404e143faa
Olivier Richard <auguste@users.gforge.inria.fr>
2010-03-06 20:44:08 +0000

    webgen's plugin system is ugly...

--- www/v3/plugin/rst_converter.rb
+++ www/v3/plugin/rst_converter.rb
@@ -6,13 +6,16 @@ class RstConverterTag < Tags::DefaultTag
 		:author => "Olivier Richard"
 		)
 
-	param 'rstfile', nil, 'The rst file to converte'
+	param 'rstfile', nil, 'The rst file to convert'
 	param 'path', '.', 'The path to rst file'
 	set_mandatory 'rstfile', true
 	register_tag 'rstconverter'
 
+  @@rst_files_already_processed = {} 
+
 	def process_tag(tag, chain)
 
+puts tag
 		rstfile = param('rstfile').split(',')
 
 		files = ""
@@ -20,14 +23,25 @@ class RstConverterTag < Tags::DefaultTag
 			files = files + " " + param('path') + '/' + filename
 		end
 
+    if !(@@rst_files_already_processed.key?(files)) then 
+#if true then
+
 #		if  File.exist?( param('path') + '/' + rstfile[0] )
 #			content = `cd rst_files ; ls `
+      puts "rstconvert files:  #{files}"
 			content = `cd rst_files ; cat #{files} | rst2html | egrep -v '^<\/body>|^<body>|^<!DOCTYPE|^<html xmlns|^<head>|^<\/head>|^<meta|^<title>|^<link|^</\html>'`
 #		else
 #		  log(:error) { "#{param('path') + '/' + rstfile[0]}  file doesn't exist"}
 #			content = ''
 #		end
-
+      @@rst_files_already_processed[files] = content
+    else
+      puts "already processed: #{files}"
+      #content = @@rst_files_already_processed[files]
+      @@rst_files_already_processed.delete(files)
+  #   puts content
+      content= ""
+    end
 		return content
 	end
 end
