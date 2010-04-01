4ffce5e1f250f4398bc706f8dbe08ceb5e28fc2e
Bruno Bzeznik <bzizou@users.gforge.inria.fr>
2010-04-01 11:22:55 +0000

    Trunk is now 2.5.0 (still have to find a name for this release)

--- Tools/oarversion.pm
+++ Tools/oarversion.pm
@@ -1,8 +1,8 @@
 package oarversion;
 require Exporter;
 
-my $OARVersion = "2.4.2";
-my $OARName = "Thriller";
+my $OARVersion = "2.5.0";
+my $OARName = "SID";
 
 sub get_version(){
     return $OARVersion." (".$OARName.")";
