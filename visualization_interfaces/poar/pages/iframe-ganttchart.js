d31565d25d62946abbf5029a13fcc4a6ac7690e8
Sundeep Kumar Mishra <sundeepkumar@users.gforge.inria.fr>
2010-08-13 18:29:34 +0000

    SVN commit

--- visualization_interfaces/poar/pages/iframe-ganttchart.js
+++
@@ -1,8 +0,0 @@
-var iframe_ganttchart = {
-  id: 'iframe-ganttchart-panel',
-  title: 'Gantt Chart',
-  frame:true,
-  xtype: 'iframepanel',
-  loadMask:true,
-  defaultSrc : 'http://localhost/drawgantt'
-};
--- visualization_interfaces/poar/pages/iframe-monika.js
+++
@@ -1,8 +0,0 @@
-var iframe_monika = {
-  id: 'iframe-monika-panel',
-  title: 'Resources Status',
-  frame:true,
-  xtype: 'iframepanel',
-  loadMask:true,
-  defaultSrc : 'http://localhost/monika'
-};
