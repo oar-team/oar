dd99603ff315ef5c5c87cd699590096abaa26783
Sundeep Kumar Mishra <sundeepkumar@users.gforge.inria.fr>
2010-08-15 10:26:17 +0000

    Final Commit

--- visualization_interfaces/poar/pages/iframe-ganttchart.js
+++
@@ -1,9 +0,0 @@
-/*Integrating the drawgantt chart */
-var iframe_ganttchart = {
-  id: 'iframe-ganttchart-panel',
-  title: 'Gantt Chart',
-  frame:true,
-  xtype: 'iframepanel',
-  loadMask:true,
-  defaultSrc : Drawgantt_URI
-};
