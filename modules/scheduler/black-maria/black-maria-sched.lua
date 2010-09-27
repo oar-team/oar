7e9d3f4eb926a999593ca626a44d5a6cb64602a0
Olivier Richard <auguste@users.gforge.inria.fr>
2010-09-27 15:38:00 +0000

    some progress for BKM

--- modules/scheduler/black-maria/black-maria-sched.lua
+++ modules/scheduler/black-maria/black-maria-sched.lua
@@ -9,6 +9,13 @@ require "pp"
 
 submitted_jobs = {}
 
+-- index correspondance for job's attributs in return of get_waiting_jobs_black_maria function 
+j_id= 1
+walltime = 2
+nb_res = 4
+modalble_id = 5
+
+
 function get_job_to_execute()
 end
 
@@ -16,39 +23,51 @@ end
 -- Jobs are submitted sequentially. It's made the assumption that there not a lot of these kind of job at a given time. 
 -- TODO: a connector for each supported LRMS (priority to SLUMR)
 function submit_to_jrms(jobs_to_launch)
-  local submit_cmd_part1 = "sbatch -N"
+  local job_ids = {}
+  local submit_cmd_part1 = "./sbatch -N"
   local submit_cmd_part2 = " -l black_maria_pilot.sh "
-  for i,job in ipair(jobs_to_launch) do
-
+  for i,job in ipairs(jobs_to_launch) do
+    job_ids[i] = job[j_id]
     print("BKM: submit job to LRMS")
-    
-    f = assert  (io.popen (submit_cmd_part1 .. job.nb_res .. 
-                           submit_cmd_part2 .. job.walltime
-                          )
-                )
+    local cmd = submit_cmd_part1 .. job[nb_res] .. submit_cmd_part2 ..
+                job[j_id] .." ".. 
+                job[walltime] .." "..
+                job[modalble_id]
+                
+    print("BKM: " .. cmd)
+
+    f = assert  (io.popen (cmd))
     for line in f:lines() do
       print(line) 
       -- retrieve foreign_jobid
       -- sbatch: Submitted batch job 469
       b,i,jid_jrms = line:find("sbatch.*%s(%d+)$")
       if b then
-        
-        -- save serialize job info
+        print("Prout:" .. jid_jrms) 
+        -- save serialize job info (
         break 
       end
     end
-
+  end
 -- update scheduler_info jobs' field to not to resubmit in next cycle
 -- TODO: build job_ids
+
   oar.set_scheduler_message_range(job_ids,"submitted to JRMS")
 end
 
+--
 -- main
-
+--
 queue = "default"
-if arg[1] then queue = arg[1]
+if arg[1] then queue = arg[1] end
 
+oar.conf_load()
+oar.connect()
+
+
+--waiting_jobs = oar.get_waiting_jobs_no_moldable(queue)
 waiting_jobs = oar.get_waiting_jobs_black_maria(queue) 
+dumptable(waiting_jobs)
 
-exit
+print(waiting_jobs[1][2])
 submit_to_jrms(waiting_jobs)
--- modules/scheduler/black-maria/black-maria-sync.lua
+++ modules/scheduler/black-maria/black-maria-sync.lua
@@ -1,24 +1,36 @@
 require "oar"
+require "pp"
 require "copas"
 
 function handler(c, host, port)
   local peer = host .. ":" .. port
   print("BKM-sync: connection from", peer)
-  c:send("Hello\r\n")
+ -- c:send("Hello\r\n")
 
-  -- get data
-  print("data from", peer, (c:receive"*l"))
+  -- get data 
+  data = (c:receive"*l")
+  print("data from", peer, data)
+  assert(loadstring("job_infos =" .. data))()
 
+  dumptable(job_infos)
+  
+  print(job_infos.yop)
+ 
   -- read JRMS' node file
 
   -- update oar's DB
+  -- save assignement
+  -- set job's stat to running 
+--  oar
+
+  
 
   -- notify almighy
 
 end
 
 -- retreive resource_ids by node 
-nodes_resource_ids = oar.get_nodes_resources_black_maria
+nodes_resources_ids = oar.get_nodes_resources_black_maria
 
 -- add tcp server
 copas.addserver(assert(socket.bind("*",2220)),
