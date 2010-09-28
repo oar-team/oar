4a56f5ba65283c277faea8540fdc91d9c5ddf73c
Olivier Richard <auguste@users.gforge.inria.fr>
2010-09-28 06:57:48 +0000

    more lines for bkm

--- modules/scheduler/black-maria/black-maria-pilot.sh
+++ modules/scheduler/black-maria/black-maria-pilot.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
+BKM_SYNC_HOST=$1
+BKM_SYN_PORT=$2
+OAR_JOB_ID=$3
+OAR_MOLDABLE_JOB_ID=$4
+WALLTIME=$5
 
+
+SLURM_NODELIST="slurm_nodelist.txt"
+
+#TODO switch case depending of foreign jrms
+NODE_FILE=$SLURM_NODELIST 
+
+#
 # notify black-maria-sync daemon
-SLURM_NODELIST
+#
+BKM_SYNC_DATA="{['j_id']=$OAR_JOB_ID, ['moldable_j_id']=$OAR_MOLDABLE_JOB_ID, ['nodes_file']='$NODE_FILE'}"
+echo $BKM_SYNC_DATA
+echo $BKM_SYNC_DATA | nc $BKM_SYNC_HOST $BKM_SYN_PORT
 
+# time to sleep
+sleep $WALLTIME
 
--- modules/scheduler/black-maria/black-maria-sync.lua
+++ modules/scheduler/black-maria/black-maria-sync.lua
@@ -14,23 +14,46 @@ function handler(c, host, port)
 
   dumptable(job_infos)
   
-  print(job_infos.yop)
+  print(job_infos.nodes_file)
  
-  -- read JRMS' node file
 
-  -- update oar's DB
-  -- save assignement
+  -- read JRMS' node file and build resource id list
+  resource_ids = {}
+  k = 1
+  local f = assert(io.open(job_infos.nodes_file, "r"))
+  for line in f:lines() do 
+    print(line)
+    for i,r_id in ipairs(nodes_resources_ids[line]) do
+      resource_ids[k]=r_id
+      k = k + 1
+    end 
+  end
+
+  dumptable(resource_ids)
+
+  -- update oar's DB / save assignement
+  --  iolib::set_assigned_moldable_job(
+  -- add_resource_job_pair
+-- TODO
+  oar.set_assigned_moldable_job(job_id,moldable_job_id)
+  oar.save_assignements_black_maria(moldable_job_id,resource_ids) 
+
+--TODO
   -- set job's stat to running 
---  oar
+--TODO
+  -- notify almighy (schedule cycle ? will update Gantt Diagram ??)
 
-  
+  -- notifiy for external tools ?
 
-  -- notify almighy
+  -- gantt visu ??? 
 
 end
 
+oar.conf_load()
+oar.connect()
 -- retreive resource_ids by node 
-nodes_resources_ids = oar.get_nodes_resources_black_maria
+nodes_resources_ids = oar.get_nodes_resources_black_maria()
+dumptable(nodes_resources_ids)
 
 -- add tcp server
 copas.addserver(assert(socket.bind("*",2220)),
