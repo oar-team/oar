94ecd14c4ac7b23f45af137460db2a8b6576a79c
Bruno Bzeznik <bzizou@users.gforge.inria.fr>
2010-10-21 17:07:09 +0000

    Made working scripts for the lustre g5k experiment

--- misc/lustre_g5kutils/lustre_client_init.bash
+++ misc/lustre_g5kutils/lustre_client_init.bash
@@ -1,2 +1,13 @@
 #!/bin/bash
 set -e
+
+if [ "$1" = "" ]
+then
+  echo "usage: $0 <mgsnode>"
+  exit 1
+fi
+
+modprobe lustre
+mkdir -p /mnt/lustre
+mount -t lustre $1@tcp0:/lustre /mnt/lustre
+
--- misc/lustre_g5kutils/lustre_mds_init.bash
+++ misc/lustre_g5kutils/lustre_mds_init.bash
@@ -1,2 +1,11 @@
 #!/bin/bash
 set -e
+
+TMP_PART=`df /tmp|tail -1|awk '{print $1}'`
+umount $TMP_PART
+pvcreate $TMP_PART
+vgcreate vg00 $TMP_PART
+lvcreate --name vg00/mdt --size 1G
+mkfs.lustre --fsname lustre --mdt --mgs /dev/vg00/mdt
+mkdir /mdt
+mount -t lustre /dev/vg00/mdt /mdt
