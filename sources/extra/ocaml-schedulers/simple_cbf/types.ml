7e23ea3b93d2899dbfcc1b5ce6b164009d62278d
auguste <olivier.richard@imag.fr>
2012-01-06 17:22:48 +0100

    remove some ocaml-schedulers

--- sources/extra/ocaml-schedulers/simple_cbf/types.ml
+++
@@ -1,9 +0,0 @@
-
-type job = {
-	mutable time_b : time_t; 
-	walltime : time_t;
-	nb_res : int;
-	mutable set_of_rs : set_of_resources;
-}
-
-
--- sources/extra/ocaml-schedulers/test/queue_on_default.sql
+++
@@ -1,1 +0,0 @@
-UPDATE `oar`.`queues` SET `state` = 'Active' WHERE CONVERT( `queues`.`queue_name` USING utf8 ) = 'default' LIMIT 1 ;
