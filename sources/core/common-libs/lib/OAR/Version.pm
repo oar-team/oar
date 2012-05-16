4eedc66006f773ead8c91885a256a4a92be1fcec
Philippe Le Brouster <philippe.le-brouster@imag.fr>
2012-05-16 15:16:53 +0200

    Imported Upstream version 2.5.1+131.g468450b

--- sources/core/common-libs/lib/OAR/Version.pm
+++ sources/core/common-libs/lib/OAR/Version.pm
@@ -1,7 +1,7 @@
 package OAR::Version;
 require Exporter;
 
-my $OARVersion = "2.5.1+130.g03edb02";
+my $OARVersion = "2.5.1+131.g468450b";
 my $OARName = "tzuhtzjo:m uy-u:xlaju:n pik";
 
 sub get_version(){
