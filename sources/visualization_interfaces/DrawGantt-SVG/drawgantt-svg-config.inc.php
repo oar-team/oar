d9697583eba21eb17cc981e959633775603a5fb2
Pierre Neyron <pierre.neyron@imag.fr>
2013-02-26 23:49:33 +0100

    [visualisation] drawgantt-svg: rework the configuration settings

--- sources/visualization_interfaces/DrawGantt-SVG/drawgantt-svg-config.inc.php
+++ sources/visualization_interfaces/DrawGantt-SVG/drawgantt-svg-config.inc.php
@@ -5,35 +5,46 @@
 // Configuration
 ////////////////////////////////////////////////////////////////////////////////
 
-$CONF['mysql_server']="localhost";
-$CONF['mysql_user']="oar_ro";
-$CONF['mysql_passwd']="oar";
-$CONF['mysql_db']="oar";
-$CONF['site'] = "My OAR resources";
-$CONF['hierarchy_resource_width'] = 10;
-$CONF['scale'] = 10;
-$CONF['time_ruler_scale'] = 6;
-$CONF['gantt_top'] = 30;
-$CONF['bottom_margin'] = 45;
-$CONF['right_margin'] = 10;
-$CONF['label_right_align'] = 105;
-$CONF['hierarchy_left_align'] = 110;
-$CONF['gantt_left_align'] = 160;
-$CONF['gantt_width'] = 1000;
-$CONF['gantt_min_job_width_for_label'] = 0;
-$CONF['resource_hierarchy'] = array('network_address','resource_id');
-$CONF['resource_properties'] = array('deploy', 'cpuset', 'besteffort', 'network_address', 'type');
-$CONF['resource_labels'] = array('network_address','cpuset');
-$CONF['state_colors'] = array('Absent' => 'url(#absentPattern)', 'Suspected' => 'url(#suspectedPattern)', 'Dead' => 'url(#deadPattern)');
-$CONF['job_colors'] = array(
+// Database access configuration
+$CONF['db_type']="mysql"; // choices: mysql for Mysql or pg for PostgreSQL
+$CONF['db_server']="127.0.0.1";
+$CONF['db_port']="3306"; // usually 3306 for Mysql or 5432 for PostgreSQL
+$CONF['db_name']="oar"; // OAR read only user account 
+$CONF['db_user']="oar";
+$CONF['db_passwd']="oar";
+$CONF['site'] = "My OAR resources"; // name for your infrastructure or site
+$CONF['resource_hierarchy'] = array( // properties to use to build the resource hierarchy drawing
+	'network_address','cpuset'); 
+$CONF['resource_labels'] = array(  // properties to use for resource labels on the left (any of the resource hierarchy or cpuset)
+	'network_address','cpuset');
+$CONF['label_display_regex'] = array( // shortening regex for labels (e.g. to shorten node-1.mycluster to node-1
+	'network_address' => '/^([^.]+)\..*$/');
+$CONF['label_cmp_regex'] = array( // string selection regex for comparing and sorting labels (resources)
+	'network_address' => '/^[^.\d]+(\d+)\..*$/');
+$CONF['resource_properties'] = array( // properties to display in the pop-up on top of the resources labels (on the left)
+	'deploy', 'cpuset', 'besteffort', 'network_address', 'type');
+$CONF['state_colors'] = array( // colors for the states of the resources in the gantt
+	'Absent' => 'url(#absentPattern)', 'Suspected' => 'url(#suspectedPattern)', 'Dead' => 'url(#deadPattern)');
+$CONF['job_colors'] = array( // colors for the types of the jobs in the gantt
 	'besteffort' => 'url(#besteffortPattern)', 
 	'deploy' => 'url(#deployPattern)', 
 	'container' => 'url(#containerPattern)', 
 	'timesharing=\w+,\w+' => 'url(#timesharingPattern)', 
 	'set_placeholder=\w+' => 'url(#placeholderPattern)',
 	);
-$CONF['short_hostname_regex'] = '/^([^.]+)\..*$/';
-$CONF['cmp_hostname_regex'] = '/^[^.\d]+(\d+)\..*$/';
+// display geometry customization
+$CONF['hierarchy_resource_width'] = 10; // default: 10
+$CONF['scale'] = 10; // default: 10
+$CONF['time_ruler_scale'] = 6; // default: 6
+$CONF['gantt_top'] = 30; // default: 30
+$CONF['bottom_margin'] = 45; // default: 45
+$CONF['right_margin'] = 10; // default 10
+$CONF['label_right_align'] = 105; // default: 105
+$CONF['hierarchy_left_align'] = 110; // default: 110
+$CONF['gantt_left_align'] = 160; // default: 160
+$CONF['gantt_width'] = 1000; // default: 1000
+$CONF['gantt_min_job_width_for_label'] = 0; // default: 0
+// pattern definitions for the state_colors and job_colors configurations
 $CONF['patterns'] = <<<EOT
 <pattern id="absentPattern" patternUnits="userSpaceOnUse" x="0" y="0" width="5" height="5" viewBox="0 0 5 5" >
 <line x1="5" y1="0" x2="0" y2="5" stroke="#000000" stroke-width="2" />
