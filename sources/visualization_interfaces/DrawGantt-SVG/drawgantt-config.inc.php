b61ebe821ce823a1b6244088ee77f57ff623ddfe
Pierre Neyron <pierre.neyron@imag.fr>
2013-05-16 22:03:21 +0200

    [drawgantt-svg] make gantt adapt to the size of the window

--- sources/visualization_interfaces/DrawGantt-SVG/drawgantt-config.inc.php
+++ sources/visualization_interfaces/DrawGantt-SVG/drawgantt-config.inc.php
@@ -1,5 +1,5 @@
 <?php
-// OAR Drawgantt SVG version
+// OAR Drawgantt SVG configuration file
 
 ////////////////////////////////////////////////////////////////////////////////
 // Configuration
@@ -13,10 +13,7 @@ $CONF['db_name']="oar"; // OAR read only user account
 $CONF['db_user']="oar";
 $CONF['db_passwd']="oar";
 $CONF['site'] = "My OAR resources"; // name for your infrastructure or site
-$CONF['resource_hierarchy'] = array( // properties to use to build the resource hierarchy drawing
-	'network_address','cpuset'); 
-$CONF['resource_labels'] = array(  // properties to use for resource labels on the left (any of the resource hierarchy or cpuset)
-	'network_address','cpuset');
+$CONF['resource_labels'] = array('network_address','cpuset'); // properties to use to describe resources (resource labels on the left)
 $CONF['cpuset_label_display_string'] = "%02d";
 $CONF['label_display_regex'] = array( // shortening regex for labels (e.g. to shorten node-1.mycluster to node-1
 	'network_address' => '/^([^.]+)\..*$/');
@@ -24,6 +21,8 @@ $CONF['label_cmp_regex'] = array( // string selection regex for comparing and so
 	'network_address' => '/^[^.\d]+(\d+)\..*$/');
 $CONF['resource_properties'] = array( // properties to display in the pop-up on top of the resources labels (on the left)
 	'deploy', 'cpuset', 'besteffort', 'network_address', 'type');
+$CONF['resource_hierarchy'] = array( // properties to use to build the resource hierarchy drawing
+	'network_address','cpuset'); 
 $CONF['state_colors'] = array( // colors for the states of the resources in the gantt
 	'Absent' => 'url(#absentPattern)', 'Suspected' => 'url(#suspectedPattern)', 'Dead' => 'url(#deadPattern)');
 $CONF['job_colors'] = array( // colors for the types of the jobs in the gantt
@@ -37,13 +36,14 @@ $CONF['job_colors'] = array( // colors for the types of the jobs in the gantt
 $CONF['hierarchy_resource_width'] = 10; // default: 10
 $CONF['scale'] = 10; // default: 10
 $CONF['time_ruler_scale'] = 6; // default: 6
-$CONF['gantt_top'] = 30; // default: 30
+$CONF['gantt_top'] = 40; // default: 40
 $CONF['bottom_margin'] = 45; // default: 45
 $CONF['right_margin'] = 10; // default 10
 $CONF['label_right_align'] = 105; // default: 105
 $CONF['hierarchy_left_align'] = 110; // default: 110
 $CONF['gantt_left_align'] = 160; // default: 160
-$CONF['gantt_width'] = 1000; // default: 1000
+$CONF['gantt_min_width'] = 1000; // default: 1000
+$CONF['gantt_min_height'] = 400; // default: 400
 $CONF['gantt_min_job_width_for_label'] = 0; // default: 0
 // pattern definitions for the state_colors and job_colors configurations
 $CONF['patterns'] = <<<EOT
