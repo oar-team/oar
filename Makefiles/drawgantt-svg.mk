d912102165cd7976a4de804047ab674f598251b0
Pierre Neyron <pierre.neyron@imag.fr>
2013-05-16 22:10:26 +0200

    [drawgantt-svg] adapt doc and install scripts to the html5 version

--- Makefiles/drawgantt-svg.mk
+++ Makefiles/drawgantt-svg.mk
@@ -1,7 +1,7 @@
 MODULE=drawgantt-svg
 SRCDIR=sources/visualization_interfaces/DrawGantt-SVG
 
-EXAMPLEDIR_FILES=$(SRCDIR)/drawgantt-svg-config.inc.php
+EXAMPLEDIR_FILES=$(SRCDIR)/drawgantt-config.inc.php
 
 PROCESS_TEMPLATE_FILES=$(SRCDIR)/drawgantt-svg.php.in
 
@@ -16,13 +16,11 @@ build: build_shared
 install: install_shared
 	install -d $(DESTDIR)$(WWWDIR)/drawgantt-svg
 	install -m 0644  $(SRCDIR)/drawgantt.html $(DESTDIR)$(WWWDIR)/drawgantt-svg/drawgantt.html
-	install -m 0644  $(SRCDIR)/drawgantt-nav.html $(DESTDIR)$(WWWDIR)/drawgantt-svg/drawgantt-nav.html
 	install -m 0644  $(SRCDIR)/drawgantt-svg.php $(DESTDIR)$(WWWDIR)/drawgantt-svg/drawgantt-svg.php
 
 uninstall: uninstall_shared
 	rm -f \
 	    $(DESTDIR)$(WWWDIR)/drawgantt-svg/drawgantt.html \
-	    $(DESTDIR)$(WWWDIR)/drawgantt-svg/drawgantt-nav.html \
 	    $(DESTDIR)$(WWWDIR)/drawgantt-svg/drawgantt-svg.php
 	
 	-rmdir \
