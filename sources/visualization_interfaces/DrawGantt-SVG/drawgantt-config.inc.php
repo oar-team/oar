f2e9e79fa3d7cb6d1aab69a7552a953a84d6e447
Pierre Neyron <pierre.neyron@imag.fr>
2013-09-16 23:36:09 +0200

    [drawgantt-svg] improve besteffort support

--- sources/visualization_interfaces/DrawGantt-SVG/drawgantt-config.inc.php
+++ sources/visualization_interfaces/DrawGantt-SVG/drawgantt-config.inc.php
@@ -45,7 +45,6 @@ $CONF['resource_hierarchy'] = array( // properties to use to build the resource 
 $CONF['state_colors'] = array( // colors for the states of the resources in the gantt
   'Absent' => 'url(#absentPattern)', 'Suspected' => 'url(#suspectedPattern)', 'Dead' => 'url(#deadPattern)', 'Standby' => 'url(#standbyPattern)');
 $CONF['job_colors'] = array( // colors for the types of the jobs in the gantt
-  'besteffort' => 'url(#besteffortPattern)', 
   'deploy' => 'url(#deployPattern)', 
   'container' => 'url(#containerPattern)', 
   'timesharing=\w+,\w+' => 'url(#timesharingPattern)', 
@@ -71,7 +70,7 @@ $CONF['gantt_min_job_width_for_label'] = 0; // default: 0
 $CONF['job_color_saturation_lightness'] = "75%,75%"; // default: "75%,75%"
 $CONF['job_color_saturation_lightness_highlight'] = "50%,50%"; // default: "50%,50%"
 $CONF['magic_number'] = (1+sqrt(5))/2; // magic number used to compute the function hue(jod_id) 
-$CONF['patterns'] = <<<EOT
+$CONF['static_patterns'] = <<<EOT
 <pattern id="absentPattern" patternUnits="userSpaceOnUse" x="0" y="0" width="5" height="5" viewBox="0 0 5 5" >
 <line x1="5" y1="0" x2="0" y2="5" stroke="#000000" stroke-width="2" />
 </pattern> 
@@ -83,9 +82,6 @@ $CONF['patterns'] = <<<EOT
 </pattern> 
 <pattern id="standbyPattern" patternUnits="userSpaceOnUse" x="0" y="0" width="5" height="5" viewBox="0 0 5 5" >
 <line x1="5" y1="0" x2="0" y2="5" stroke="#00ff00" stroke-width="2" />
-</pattern> 
-<pattern id="besteffortPattern" patternUnits="userSpaceOnUse" x="0" y="0" width="20" height="20" viewBox="0 0 20 20" >
-<text font-size="10" x="0" y="10" fill="#888888">B</text>
 </pattern> 
 <pattern id="containerPattern" patternUnits="userSpaceOnUse" x="0" y="0" width="20" height="20" viewBox="0 0 20 20" >
 <text font-size="10" x="0" y="20" fill="#888888">C</text>
@@ -100,6 +96,17 @@ $CONF['patterns'] = <<<EOT
 <pattern id="timesharingPattern" patternUnits="userSpaceOnUse" x="0" y="0" width="20" height="20" viewBox="0 0 20 20" >
 <text font-size="10" x="10" y="20" fill="#888888">T</text>
 </pattern> 
+EOT;
+
+// Besteffort job type display options
+$CONF['besteffort_truncate_job_to_now'] = 1; // default: 1
+$CONF['besteffort_label'] = "(%%LABEL%%)"; // default: "(%%LABEL%%)"
+$CONF['besteffort_pattern'] = <<<EOT
+<pattern id="%%PATTERN_ID%%" patternUnits="userSpaceOnUse" x="0" y="0" width="10" height="10" viewBox="0 0 10 10" >
+<line x1="0" y1="0" x2="10" y2="10" stroke="%%PATTERN_COLOR%%" stroke-width="4"/>
+<line x1="-5" y1="5" x2="5" y2="15" stroke="%%PATTERN_COLOR%%" stroke-width="4"/>
+<line x1="5" y1="-5" x2="15" y2="5" stroke="%%PATTERN_COLOR%%" stroke-width="4"/>
+</pattern>'
 EOT;
 
 // Debugging
