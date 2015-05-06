ae4e77e5a32f391642f39828b996a21cb4a55b27
Pierre Neyron <pierre.neyron@imag.fr>
2015-05-06 23:51:40 +0200

    fix top-cpuset

--- misc/top_cpuset.sh
+++ misc/top_cpuset.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+CGROOT=${1:-/dev/cpuset/oar}
 while true; do
   clear
-  if [ -d /dev/cpuset/oar ]; then
-    find /dev/cpuset/oar \( -name cpuset.cpus -o -name tasks \) -exec grep -H "" {} \;
+  if [ -d $CGROOT ]; then
+    find $CGROOT \( -name cpuset.cpus -o -name tasks -o -name cpuset.mems \) -exec grep -H "" {} \;
   else
-    echo "/dev/cpuset/oar not created yet..."
+    echo "no $CGROOT"
   fi
   sleep 1
 done
