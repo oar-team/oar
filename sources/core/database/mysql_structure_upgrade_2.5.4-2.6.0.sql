90367333a53705340f1d0ac496c4af871e0180d2
Pierre Neyron <pierre.neyron@imag.fr>
2015-05-28 23:55:49 +0200

    [database] drop a remaining mysql related file

--- sources/core/database/mysql_structure_upgrade_2.5.4-2.6.0.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE job_dependencies ADD min_start_shift  VARCHAR(12) NOT NULL DEFAULT "";
-ALTER TABLE job_dependencies ADD max_start_shift  VARCHAR(12) NOT NULL DEFAULT "";
-
--- Update the database schema version
-DELETE FROM `schema`;
-INSERT INTO `schema`(version, name) VALUES ('2.6.0', '');
-
