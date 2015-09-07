032670d5828b489037c01a9024e7ca91638bbd52
Pierre Neyron <pierre.neyron@imag.fr>
2015-09-07 21:57:10 +0200

    [lib] bump version -> 2.5.5

--- sources/core/common-libs/lib/OAR/Version.pm
+++ sources/core/common-libs/lib/OAR/Version.pm
@@ -1,8 +1,8 @@
 package OAR::Version;
 require Exporter;
 
-my $OARVersion = "2.5.4";
-my $OARName = "Froggy Summer";
+my $OARVersion = "2.5.5";
+my $OARName = "The Force Awakens";
 
 sub get_version(){
     return $OARVersion." (".$OARName.")";
