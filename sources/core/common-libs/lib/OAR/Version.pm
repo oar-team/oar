115bd890e9640243f37df019349b046947134128
Pierre Neyron <pierre.neyron@imag.fr>
2015-10-21 13:57:11 +0200

    [lib] prepare 2.5.6, bump version

--- sources/core/common-libs/lib/OAR/Version.pm
+++ sources/core/common-libs/lib/OAR/Version.pm
@@ -1,8 +1,8 @@
 package OAR::Version;
 require Exporter;
 
-my $OARVersion = "2.5.5";
-my $OARName = "The Force Awakens";
+my $OARVersion = "2.5.6";
+my $OARName = "Winter is coming";
 
 sub get_version(){
     return $OARVersion." (".$OARName.")";
