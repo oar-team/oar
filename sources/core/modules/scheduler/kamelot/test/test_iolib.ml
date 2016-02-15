f78393e40b9c346141d90f0589a220bf109a6e5a
Pierre Neyron <pierre.neyron@imag.fr>
2016-02-15 16:42:13 +0100

    [scheduler] remove kamelot (OCaml) code.

--- sources/core/modules/scheduler/kamelot/test/test_iolib.ml
+++
@@ -1,5 +0,0 @@
-open Iolib
-let _= 
-(*  let conn = let r = Iolib_pg.connect () in at_exit (fun () -> Iolib_pg.disconnect r); r in *)
- (* let r = Iolib_pg.connect () in let g = get_resource_list r in test_db r ;; *)
- let r = Iolib.connect () in get_resource_list r  ;;  
