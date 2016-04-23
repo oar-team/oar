6fba864e21d46be2d6a8e67e730c5ee92f82e75a
Pierre Neyron <pierre.neyron@imag.fr>
2016-04-23 22:49:36 +0200

    [monika] revert to 2.5 makefiles/setup/config

--- Makefiles/monika.mk
+++ Makefiles/monika.mk
@@ -4,8 +4,9 @@ SRCDIR=sources/visualization_interfaces/Monika
 OAR_PERLLIB=$(SRCDIR)/lib
 
 SHAREDIR_FILES= $(SRCDIR)/monika.conf.in \
-		  $(SRCDIR)/userInfos.cgi \
-		  $(SRCDIR)/monika.cgi.in \
+		  $(SRCDIR)/userInfos.cgi
+
+CGIDIR_FILES = $(SRCDIR)/monika.cgi.in
 
 
 WWWDIR_FILES = $(SRCDIR)/monika.css
@@ -13,19 +14,15 @@ WWWDIR_FILES = $(SRCDIR)/monika.css
 include Makefiles/shared/shared.mk
 
 clean: clean_shared
-	# Nothing to do
+# Nothing to do
 
 build: build_shared
-	# Nothing to do
+# Nothing to do
 
 install: install_shared
-	install -d $(DESTDIR)$(CGIDIR)/monika
-	install -m 0644  $(SRCDIR)/monika.cgi $(DESTDIR)$(CGIDIR)/monika/monika.cgi
+# Nothing to do
 
 uninstall: uninstall_shared
-	rm -f \
-	    $(DESTDIR)$(CGIDIR)/monika/monika.cgi
-	-rmdir \
-	    $(DESTDIR)$(CGIDIR)/monika
+# Nothing to do
 
 .PHONY: install setup uninstall build clean
