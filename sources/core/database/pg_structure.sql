4145076e50db3f647a2bccf0d7a88ec1bc84c385
Pierre Neyron <pierre.neyron@imag.fr>
2022-04-13 10:53:32 +0200

    [database] fix database SQL files after oarwalltime changes

--- sources/core/database/pg_structure.sql
+++ sources/core/database/pg_structure.sql
@@ -4,7 +4,7 @@ CREATE TABLE schema (
   version VARCHAR( 255 ) NOT NULL,
   name VARCHAR( 255 ) NOT NULL
 );
-INSERT INTO schema VALUES ('2.5.8','');
+INSERT INTO schema VALUES ('2.5.10','');
 
 CREATE TABLE accounting (
   window_start integer NOT NULL ,
@@ -322,7 +322,7 @@ CREATE TABLE walltime_change (
   granted_with_force integer NOT NULL default '0',
   granted_with_delay_next_jobs integer NOT NULL default '0',
   granted_with_whole integer NOT NULL default '0',
-  timeout integer NOT NULL default '3600',
+  timeout integer NOT NULL default '0',
   PRIMARY KEY (job_id)
 );
 CREATE INDEX walltime_change_job_id ON walltime_change (job_id);
