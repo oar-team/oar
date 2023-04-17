oar-team/oar

commit 4aa85773207ffc867817bf39d8d0fdb2188c16b4

[IO.pm] get manage_remote_commands() from Tools.pm for better logging
Tools::manage_remote_commands() is responsible of launching remote
commands with Taktuk. Outputs (stderr, stdout) were not using OAR's
logging system, causing not correctly formarted lines in oar.log file.

Tools.pm is a requirement of oarexec, thus it is piped with oarexec by
Taktuk on remote hosts. This means Tools.pm can't depend on other OAR
modules, like Judas (which ships the logging subroutines like
oar_info()) therefore OAR's logging subroutines aren't usable in
manage_remote_commands().
This subroutine being not a requirement of oarexec, we can move it to
another module (IO) to use OAR's logging subroutines.

sources/core/common-libs/lib/OAR/IO.pm
@@ -221,6 +221,9 @@ sub sql_count($$);
 sub sql_select($$$$);
 sub inserts_from_file($$$);
 
+# REMOTE EXECUTION FUNCTIONS
+sub manage_remote_commands($$$$$$$$$);
+
 # PERL HELPER FUNCTIONS
 sub array_minus(\@@);
 
@@ -8778,7 +8781,7 @@ sub job_finishing_sequence($$$$$$$){
                     project             => $job->{project},
                     log_level => OAR::Modules::Judas::get_log_level()
                 };
-                my ($tag,@bad) = OAR::Tools::manage_remote_commands([keys(%{$cpuset_nodes})],$cpuset_data_hash,$cpuset_file,"clean",$openssh_cmd,$taktuk_cmd,$dbh);
+                my ($tag,@bad) = manage_remote_commands([keys(%{$cpuset_nodes})],$cpuset_data_hash,$cpuset_file,"clean",$openssh_cmd,$taktuk_cmd,$dbh,$Module_name,$Session_id);
                 if ($tag == 0){
                     my $str = "[JOB FINISHING SEQUENCE] [CPUSET] [$job_id] Bad cpuset file: $cpuset_file\n";
                     oar_error("JobFinishingSequence", "Bad cpuset file: $cpuset_file\n", $session_id, $job_id);
@@ -8889,6 +8892,128 @@ sub array_minus(\@@) {
     my @b = @_;
     my %e = map{ $_ => undef } @b;
     return grep( ! exists( $e{$_} ), @$a );
+}
+
+# Manage commands on several nodes like cpuset or suspend job
+# args: array of host to connect to, hashtable to transfer, name of the file containing the perl script, action to perform (start or stop), SSH command to use, taktuk cmd or undef, database ref
+sub manage_remote_commands($$$$$$$$$){
+    my $connect_hosts = shift;
+    my $data_hash = shift;
+    my $manage_file = shift;
+    my $action = shift;
+    my $ssh_cmd = shift;
+    my $taktuk_cmd = shift;
+    my $base = shift;
+    my $module_name = shift;
+    my $session_id = shift;
+
+    my @bad;
+    $ssh_cmd = OAR::Tools::get_default_openssh_cmd() if (!defined($ssh_cmd));
+    # Prepare commands to run on each node
+    my $string_to_transfer;
+    open(FILE, $manage_file) or return(0,undef);
+    while(<FILE>){
+        $string_to_transfer .= $_;
+    }
+    close(FILE);
+    $string_to_transfer .= "__END__\n";
+    # suitable Data::Dumper configuration for serialization
+    $Data::Dumper::Purity = 1;
+    $Data::Dumper::Terse = 1;
+    $Data::Dumper::Indent = 0;
+    $Data::Dumper::Deepcopy = 1;
+
+    $string_to_transfer .= Dumper($data_hash);
+
+    if (!defined($taktuk_cmd)){
+        # Dispatch via sentinelle
+        my @node_commands;
+        my @node_corresponding;
+        foreach my $n (@{$connect_hosts}){
+            my $cmd = "$ssh_cmd -x -T $n TAKTUK_HOSTNAME=$n perl - $action";
+            push(@node_commands, $cmd);
+            push(@node_corresponding, $n);
+        }
+        my @bad_tmp = sentinelle(10,get_ssh_timeout(), \@node_commands, $string_to_transfer,$base);
+        foreach my $b (@bad_tmp){
+            push(@bad, $node_corresponding[$b]);
+        }
+    }else{
+        # Dispatch via taktuk
+        my %tmp_node_hash;
+
+        pipe(tak_node_read,tak_node_write);
+        pipe(tak_stdin_read,tak_stdin_write);
+        pipe(tak_stdout_read,tak_stdout_write);
+        my $pid = fork;
+        if($pid == 0){
+            #CHILD
+            $SIG{CHLD} = 'DEFAULT';
+            $SIG{TERM} = 'DEFAULT';
+            $SIG{INT}  = 'DEFAULT';
+            $SIG{QUIT} = 'DEFAULT';
+            $SIG{USR1} = 'DEFAULT';
+            $SIG{USR2} = 'DEFAULT';
+            my $cmd = "$taktuk_cmd -c '$ssh_cmd' ".'-o status=\'"STATUS $host $line\n"\''." -f '<&=".fileno(tak_node_read)."' broadcast exec [ perl - $action ], broadcast input file [ - ], broadcast input close";
+            fcntl(tak_node_read, F_SETFD, 0);
+            close(tak_node_write);
+            close(tak_stdout_read);
+            close(STDOUT);
+            # Redirect taktuk output into the pipe
+            open(STDOUT, ">& tak_stdout_write");
+        
+            # Use the child STDIN to send the user command
+            close(tak_stdin_write);
+            close(STDIN);
+            open(STDIN, "<& tak_stdin_read");
+
+            exec($cmd);
+            warn("[ERROR] Cannot execute $cmd.\n");
+            exit(-1);
+        }
+        close(tak_node_read);
+        close(tak_stdin_read);
+        close(tak_stdout_write);
+
+        # Send node list
+        foreach my $n (@{$connect_hosts}){
+            $tmp_node_hash{$n} = 1;
+            print(tak_node_write "$n\n");
+        }
+        close(tak_node_write);
+       
+        eval{
+            $SIG{ALRM} = sub { kill(19,$pid); die "alarm\n" };
+            alarm(OAR::Tools::get_taktuk_timeout());     
+            # Send data structure to all nodes
+            print(tak_stdin_write $string_to_transfer);
+            close(tak_stdin_write);
+            # Check good nodes from the stdout taktuk
+            while(<tak_stdout_read>){
+                if ($_ =~ /^STATUS ([\w\.\-\d]+) (\d+)$/){
+                    if ($2 == 0){
+                        delete($tmp_node_hash{$1}) if (defined($tmp_node_hash{$1}));
+                    }
+                }else{
+                    #print("[TAKTUK OUTPUT] $_");
+                    oar_info($module_name, "[TAKTUK OUTPUT] $_", $session_id);
+                }
+            }
+            close(tak_stdout_read);
+            waitpid($pid,0);
+            alarm(0);
+        };
+        if ($@){
+            if (defined($pid)){
+                # Kill all taktuk children
+                my ($children,$cmd_name) = get_one_process_children($pid);
+                kill(9,@{$children});
+            }
+        }
+        @bad = keys(%tmp_node_hash);
+    }
+
+    return(1,@bad);
 }
 
 # END OF THE MODULE

sources/core/common-libs/lib/OAR/Tools.pm
@@ -750,125 +750,6 @@ sub check_resource_system_property($){
 }
 
 
-# Manage commands on several nodes like cpuset or suspend job
-# args: array of host to connect to, hashtable to transfer, name of the file containing the perl script, action to perform (start or stop), SSH command to use, taktuk cmd or undef, database ref
-sub manage_remote_commands($$$$$$$){
-    my $connect_hosts = shift;
-    my $data_hash = shift;
-    my $manage_file = shift;
-    my $action = shift;
-    my $ssh_cmd = shift;
-    my $taktuk_cmd = shift;
-    my $base = shift;
-    
-    my @bad;
-    $ssh_cmd = $Default_openssh_cmd if (!defined($ssh_cmd));
-    # Prepare commands to run on each node
-    my $string_to_transfer;
-    open(FILE, $manage_file) or return(0,undef);
-    while(<FILE>){
-        $string_to_transfer .= $_;
-    }
-    close(FILE);
-    $string_to_transfer .= "__END__\n";
-    # suitable Data::Dumper configuration for serialization
-    $Data::Dumper::Purity = 1;
-    $Data::Dumper::Terse = 1;
-    $Data::Dumper::Indent = 0;
-    $Data::Dumper::Deepcopy = 1;
-
-    $string_to_transfer .= Dumper($data_hash);
-
-    if (!defined($taktuk_cmd)){
-        # Dispatch via sentinelle
-        my @node_commands;
-        my @node_corresponding;
-        foreach my $n (@{$connect_hosts}){
-            my $cmd = "$ssh_cmd -x -T $n TAKTUK_HOSTNAME=$n perl - $action";
-            push(@node_commands, $cmd);
-            push(@node_corresponding, $n);
-        }
-        my @bad_tmp = sentinelle(10,get_ssh_timeout(), \@node_commands, $string_to_transfer,$base);
-        foreach my $b (@bad_tmp){
-            push(@bad, $node_corresponding[$b]);
-        }
-    }else{
-        # Dispatch via taktuk
-        my %tmp_node_hash;
-
-        pipe(tak_node_read,tak_node_write);
-        pipe(tak_stdin_read,tak_stdin_write);
-        pipe(tak_stdout_read,tak_stdout_write);
-        my $pid = fork;
-        if($pid == 0){
-            #CHILD
-            $SIG{CHLD} = 'DEFAULT';
-            $SIG{TERM} = 'DEFAULT';
-            $SIG{INT}  = 'DEFAULT';
-            $SIG{QUIT} = 'DEFAULT';
-            $SIG{USR1} = 'DEFAULT';
-            $SIG{USR2} = 'DEFAULT';
-            my $cmd = "$taktuk_cmd -c '$ssh_cmd' ".'-o status=\'"STATUS $host $line\n"\''." -f '<&=".fileno(tak_node_read)."' broadcast exec [ perl - $action ], broadcast input file [ - ], broadcast input close";
-            fcntl(tak_node_read, F_SETFD, 0);
-            close(tak_node_write);
-            close(tak_stdout_read);
-            close(STDOUT);
-            # Redirect taktuk output into the pipe
-            open(STDOUT, ">& tak_stdout_write");
-        
-            # Use the child STDIN to send the user command
-            close(tak_stdin_write);
-            close(STDIN);
-            open(STDIN, "<& tak_stdin_read");
-
-            exec($cmd);
-            warn("[ERROR] Cannot execute $cmd.\n");
-            exit(-1);
-        }
-        close(tak_node_read);
-        close(tak_stdin_read);
-        close(tak_stdout_write);
-
-        # Send node list
-        foreach my $n (@{$connect_hosts}){
-            $tmp_node_hash{$n} = 1;
-            print(tak_node_write "$n\n");
-        }
-        close(tak_node_write);
-       
-        eval{
-            $SIG{ALRM} = sub { kill(19,$pid); die "alarm\n" };
-            alarm(OAR::Tools::get_taktuk_timeout());     
-            # Send data structure to all nodes
-            print(tak_stdin_write $string_to_transfer);
-            close(tak_stdin_write);
-            # Check good nodes from the stdout taktuk
-            while(<tak_stdout_read>){
-                if ($_ =~ /^STATUS ([\w\.\-\d]+) (\d+)$/){
-                    if ($2 == 0){
-                        delete($tmp_node_hash{$1}) if (defined($tmp_node_hash{$1}));
-                    }
-                }else{
-                    print("[TAKTUK OUTPUT] $_");
-                }
-            }
-            close(tak_stdout_read);
-            waitpid($pid,0);
-            alarm(0);
-        };
-        if ($@){
-            if (defined($pid)){
-                # Kill all taktuk children
-                my ($children,$cmd_name) = get_one_process_children($pid);
-                kill(9,@{$children});
-            }
-        }
-        @bad = keys(%tmp_node_hash);
-    }
-
-    return(1,@bad);
-}
-
 # read a line on a socket
 # arg1 --> socket
 # arg2 --> timeout

sources/core/modules/node_change_state.pl
@@ -277,7 +277,7 @@ foreach my $i (@events_to_check){
                         my $suspend_file = get_conf("SUSPEND_RESUME_FILE");
                         $suspend_file = OAR::Tools::get_default_suspend_resume_file() if (!defined($suspend_file));
                         $suspend_file = "$ENV{OARDIR}/$suspend_file" if ($suspend_file !~ /^\//);
-                        my ($tag,@bad) = OAR::Tools::manage_remote_commands([keys(%{$cpuset_nodes})],$suspend_data_hash,$suspend_file,"suspend",$openssh_cmd,$taktuk_cmd,$base);
+                        my ($tag,@bad) = OAR::IO::manage_remote_commands([keys(%{$cpuset_nodes})],$suspend_data_hash,$suspend_file,"suspend",$openssh_cmd,$taktuk_cmd,$base,$Module_name,$Session_id);
                         if ($tag == 0){
                             my $log_str = "[SUSPEND_RESUME] [$i->{job_id}] bad suspend/resume file: $suspend_file\n";
                             my $str = "[NodeChangeState] $log_str";

sources/core/modules/runner/bipbip.in
@@ -276,7 +276,7 @@ if ((!defined($types->{deploy})) and (!(defined($types->{cosystem})) and ($#host
     };
     if (defined($cpuset_nodes)){
         my $taktuk_cmd = get_conf("TAKTUK_CMD");
-        my ($tag,@bad_tmp) = OAR::Tools::manage_remote_commands([keys(%{$cpuset_nodes})],$cpuset_data_hash,$Cpuset_file,"init",$Openssh_cmd,$taktuk_cmd,$base);
+        my ($tag,@bad_tmp) = OAR::IO::manage_remote_commands([keys(%{$cpuset_nodes})],$cpuset_data_hash,$Cpuset_file,"init",$Openssh_cmd,$taktuk_cmd,$base,$Module_name,$Session_id);
         if ($tag == 0){
             my $log_str = "Bad cpuset file: $Cpuset_file\n";
             my $str = "[bipbip $Job_id] [CPUSET] $log_str";
@@ -314,7 +314,7 @@ if ((!defined($types->{deploy})) and (!(defined($types->{cosystem})) and ($#host
                         delete($cpuset_nodes->{$b});
                     }
                     $cpuset_data_hash->{nodes}=$cpuset_nodes;
-                    my ($tag,@bad_tmp) = OAR::Tools::manage_remote_commands([keys(%{$cpuset_nodes})],$cpuset_data_hash,$Cpuset_file,"clean",$Openssh_cmd,$taktuk_cmd,$base);
+                    my ($tag,@bad_tmp) = OAR::IO::manage_remote_commands([keys(%{$cpuset_nodes})],$cpuset_data_hash,$Cpuset_file,"clean",$Openssh_cmd,$taktuk_cmd,$base,$Module_name,$Session_id);
                     push(@bad, @bad_tmp);
                 }
             }

sources/core/modules/scheduler/oar_meta_sched
@@ -1204,7 +1204,7 @@ foreach my $j (OAR::IO::get_jobs_in_state($base, "Resuming")){
                     my $suspend_file = get_conf("SUSPEND_RESUME_FILE");
                     $suspend_file = OAR::Tools::get_default_suspend_resume_file() if (!defined($suspend_file));
                     $suspend_file = "$ENV{OARDIR}/$suspend_file" if ($suspend_file !~ /^\//);
-                    my ($tag,@bad) = OAR::Tools::manage_remote_commands([keys(%{$cpuset_nodes})],$suspend_data_hash,$suspend_file,"resume",$Openssh_cmd,$taktuk_cmd,$base);
+                    my ($tag,@bad) = OAR::IO::manage_remote_commands([keys(%{$cpuset_nodes})],$suspend_data_hash,$suspend_file,"resume",$Openssh_cmd,$taktuk_cmd,$base,$Module_name,$Session_id);
                     if ($tag == 0){
                         my $log_str = "Bad suspend/resume file: $suspend_file\n";
                         my $str = "[MetaSched] [SUSPEND_RESUME] [$j->{job_id}] $log_str\n";

commit 368734cdae3f7f98e41621912b61b75d978a217f

[IO] Fix undefined OAR::Tools functions: get_ssh_timeout, sentinelle, and get_one_preocess_children
Issues caused by commit 4aa857732, which moved the
manage_remote_commands function from OAR::Tools to OAR::IO.

Bug does not happen if TAKTUK_CMD is configured.

sources/core/common-libs/lib/OAR/IO.pm
@@ -10141,8 +10141,8 @@ sub manage_remote_commands($$$$$$$$$) {
             push(@node_commands,      $cmd);
             push(@node_corresponding, $n);
         }
-        my @bad_tmp =
-          sentinelle(10, get_ssh_timeout(), \@node_commands, $string_to_transfer, $base);
+        my @bad_tmp = OAR::Tools::sentinelle(10, OAR::Tools::get_ssh_timeout(),
+            \@node_commands, $string_to_transfer, $base);
         foreach my $b (@bad_tmp) {
             push(@bad, $node_corresponding[$b]);
         }
@@ -10224,7 +10224,7 @@ sub manage_remote_commands($$$$$$$$$) {
             if (defined($pid)) {
 
                 # Kill all taktuk children
-                my ($children, $cmd_name) = get_one_process_children($pid);
+                my ($children, $cmd_name) = OAR::Tools::get_one_process_children($pid);
                 kill(9, @{$children});
             }
         }