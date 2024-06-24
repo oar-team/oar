4d900cc546344c88390aa3083cc6746813bd5b50
Pierre Neyron <pierre.neyron@imag.fr>
2024-06-24 18:48:10 +0200

    bpftool

--- sources/core/tools/oarcgdev/setup.sh
+++ sources/core/tools/oarcgdev/setup.sh
@@ -7,6 +7,6 @@ kexec-tools	kexec-tools/load_kexec	boolean	true
 kexec-tools	kexec-tools/use_grub_config	boolean	true
 EOF
 
-apt install --yes libbpf-dev clang make bash-completion kexec-tools nvidia-kernel-dkms
+apt install --yes libbpf-dev clang make bash-completion kexec-tools nvidia-kernel-dkms bpftool
 
 reboot
