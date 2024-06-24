9d9580e0e25c723805b76aa9ba6dee7b5f34d7c3
Pierre Neyron <pierre.neyron@imag.fr>
2024-06-24 16:56:49 +0200

    ok

--- sources/core/tools/oarcgdev/oarcgdev.c
+++ sources/core/tools/oarcgdev/oarcgdev.c
@@ -16,8 +16,6 @@
 
 #define DEV_CGROUP_PROG "./oarcgdev-ebpf.o"
 
-#define TEST_CGROUP "/test-bpf-based-device-cgroup/"
-
 #include <linux/limits.h>
 #include <unistd.h>
 #include <sys/mount.h>
@@ -28,14 +26,15 @@
 	__FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
 /*
- * Usage: oarcgdev <bpffs_filename> <cgroup_path> <dev> [<dev> [<dev> [...]]]
+ * Usage: oarcgdev <bpffs_path> <cgroup_path> <dev> [<dev> [<dev> [...]]]
  * Where dev is in the form: devtype:major:minor
  */
 int main(int argc, char **argv)
 {
 	struct bpf_object *obj;
+	struct bpf_program *prog;
 	int error = EXIT_FAILURE;
-	int prog_fd, denymap_fd, cgroup_fd;
+	int cgroup_fd, cgroup_procs_fd;
 	__u64 denykeys[32];
 	int extra_prog_load_log_flags = 0;
 
@@ -43,9 +42,9 @@ int main(int argc, char **argv)
 		fprintf(stderr, "Error: %s requires at least 3 parameters\n", argv[0]);
 		return error;
 	}
-	const char* bpffs_filename = argv[1];
+	const char* bpffs_path = argv[1];
 	const char* cgroup_path = argv[2];
-	printf("bpffs: %s, cgroup: %s\n", bpffs_filename, cgroup_path);
+	printf("bpffs: %s, cgroup: %s\n", bpffs_path, cgroup_path);
 
 
 	for (int i = 0; i < (argc - 3); i++) {
@@ -63,7 +62,7 @@ int main(int argc, char **argv)
 			type = BPF_DEVCG_DEV_CHAR;
 			break;
 		default:
-			fprintf(stderr, "Error: %s is not a char device, other are not supported\n", argv[i]);
+			fprintf(stderr, "Error: %s is not a block or a character device, other are not supported\n", argv[i]);
 			return error;
 		}
 		denykeys[i] = make_denykey(type, major(s.st_rdev), minor(s.st_rdev));
@@ -77,54 +76,49 @@ int main(int argc, char **argv)
 	LIBBPF_OPTS(bpf_object_open_opts, opts,
 		.kernel_log_level = extra_prog_load_log_flags,
 	);
-	struct bpf_program *prog;
 
 	obj = bpf_object__open_file(DEV_CGROUP_PROG, &opts);
 	if (!obj) {
-		printf("Failed to open program\n");
+		printf("Failed to open BPF object\n");
 		return -errno;
 	}
 
 	prog = bpf_object__next_program(obj, NULL);
 	if (!prog) {
-		printf("Failed to get program\n");
+		printf("Failed to get BPF program\n");
 		return -ENOENT;
 	}
 
 	bpf_program__set_type(prog, BPF_PROG_TYPE_CGROUP_DEVICE);
 	if ((error = bpf_object__load(obj))) {
-		printf("Failed to load program\n");
+		printf("Failed to load BPF program\n");
 		bpf_object__close(obj);
 		return(error);
 	}
 
-	if ((error = bpf_program__pin(prog, "/sys/fs/bpf/toto42")) < 0 ) {
-		printf("Failed to pin program\n");
+	if ((error = bpf_program__pin(prog, bpffs_path)) < 0 ) {
+		printf("Failed to pin BPF program\n");
 		bpf_object__close(obj);
 		return(error);
 	}
 
-	prog_fd = bpf_program__fd(prog);
+	struct bpf_map *denymap;
 
-	struct bpf_map *map;
-
-	if (!(map = bpf_object__find_map_by_name(obj, "denymap"))) {
-		printf("Failed to find map\n");
+	//bpf_map_create(BPF_MAP_TYPE_HASH, "denymap", 
+	if (!(denymap = bpf_object__find_map_by_name(obj, "denymap"))) {
+		printf("Failed to find BPF map\n");
 		bpf_object__close(obj);
 		return -ENOENT;
 	}
 
-	denymap_fd = bpf_map__fd(map);
-
 	__u8 denyvalue = 0;
 	for (int i = 0; i < (argc - 3); i++) {
-		if (bpf_map_update_elem(denymap_fd, &denykeys[i], &denyvalue, 0)) {
+		if (bpf_map__update_elem(denymap, &denykeys[i], sizeof(denykeys[i]), &denyvalue, sizeof(denyvalue), BPF_ANY)) {
 			printf("Failed to write in map\n");
 			return -EINVAL;
 		}
 	}
 
-	int fd;
 	char cgroup_procs_path[PATH_MAX + 1];
 	pid_t pid = getpid();
 
@@ -137,21 +131,21 @@ int main(int argc, char **argv)
 	snprintf(cgroup_procs_path, sizeof(cgroup_procs_path),
 		 "%s/cgroup.procs", cgroup_path);
 
-	fd = open(cgroup_procs_path, O_WRONLY);
-	if (fd < 0) {
+	cgroup_procs_fd = open(cgroup_procs_path, O_WRONLY);
+	if (cgroup_procs_fd < 0) {
 		fprintf(stderr, "Failed to open cgroup procs\n");
-		return fd;
+		return cgroup_procs_fd;
 	}
 
-	if (dprintf(fd, "%d\n", pid) < 0) {
+	if (dprintf(cgroup_procs_fd, "%d\n", pid) < 0) {
 		fprintf(stderr, "Failed to joining cgroup\n");
 		return -EINVAL;
 	}
 
-	close(fd);
+	close(cgroup_procs_fd);
 
 	/* Attach bpf program */
-	if (bpf_prog_attach(prog_fd, cgroup_fd, BPF_CGROUP_DEVICE, 0)) {
+	if (bpf_prog_attach(bpf_program__fd(prog), cgroup_fd, BPF_CGROUP_DEVICE, 0)) {
 		printf("Failed to attach DEV_CGROUP program");
 		return -EINVAL;
 	}
