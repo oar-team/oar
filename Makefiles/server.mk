083724587deccd97fcc7788504ba445b721f63b0
Pierre Neyron <pierre.neyron@imag.fr>
2025-01-12 22:50:05 +0100

    [Makefile] Install suspend_resume_manager_systemd.pl

--- Makefiles/server.mk
+++ Makefiles/server.mk
@@ -42,6 +42,7 @@ SHAREDIR_FILES = $(SRCDIR)/tools/job_resource_manager.pl \
                    $(SRCDIR)/tools/job_resource_manager_cgroups.pl \
                    $(SRCDIR)/tools/job_resource_manager_systemd.pl \
 		   $(SRCDIR)/tools/suspend_resume_manager.pl \
+		   $(SRCDIR)/tools/suspend_resume_manager_systemd.pl \
 		   $(SRCDIR)/tools/oarmonitor_sensor.pl \
 		   $(SRCDIR)/scripts/server_epilogue \
 		   $(SRCDIR)/scripts/server_prologue \
